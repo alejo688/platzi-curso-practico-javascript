a9d504c0d9c69e198bf4459daaf3002fff223c2b
Alejandro Ramirez <alejandroramirez688@Gmail.com>
2021-06-29 22:41:12 -0500

    Se adicionaron los calculos del perimetro y area de las figuras geometricas

--- figuras.js
+++ figuras.js
@@ -1,1 +1,56 @@
-console.log("Hello world")
+// Código del cuadrado
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+console.log(`Los lados del cuadrado mide: ${ladoCuadrado} cm`);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`El perímetro del cuadrado es: ${perimetroCuadrado} cm`);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log(`El área del cuadrado es: ${areaCuadrado} cm^2`);
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log(`Los lados del triángulo mide: ${ladoTriangulo1} cm, ${ladoTriangulo2} cm, ${baseTriangulo} cm`);
+
+const alturaTriangulo = 5.5;
+console.log(`La altura lados del triángulo mide: ${alturaTriangulo} cm`);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log(`El perímetro del triángulo es: ${perimetroTriangulo} cm`);
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log(`El área del triángulo es: ${areaTriangulo} cm^2`);
+
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculos");
+
+// Radio
+const radioCirculo = 4;
+console.log(`El radio del círculo mide: ${radioCirculo} cm`);
+
+// Diámetro
+const diametroCirculo = radioCirculo * 2;
+console.log(`El diametro del círculo mide: ${diametroCirculo} cm`);
+
+// PI
+
+const PI = Math.PI;
+console.log(`PI es: ${PI}`);
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log(`El perimetro del círculo mide: ${perimetroCirculo} cm`);
+
+//Área
+
+const areaCirculo = (radioCirculo * radioCirculo) * PI;
+console.log(`El área del círculo mide: ${areaCirculo} cm^2`);
+
+console.groupEnd();
