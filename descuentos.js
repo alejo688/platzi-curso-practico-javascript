a0eb7ba0be4ec6f9ecbb6ba2ecdbb4a1ab116bda
Alejandro Ramirez <alejandroramirez688@Gmail.com>
2021-07-12 23:52:05 -0500

    Se adiciona calculo de descuento por cupon

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,30 @@
+function validarCupon(codigo) {
+    const cupones = [
+        {
+            codigo: "PQEK-9932-SD21",
+            descuento: 20
+        },
+        {
+            codigo: "PQEK-76E1-FE87",
+            descuento: 30
+        },
+        {
+            codigo: "PQEK-54FS-JH75",
+            descuento: 15
+        },
+        {
+            codigo: "PQEK-32FS-GF76",
+            descuento: 10
+        },
+    ];
+
+    const existeCupon = cupones.find(function(cupon){
+        return cupon.codigo === codigo;
+    });
+
+    return existeCupon;
+}
+
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -15,5 +42,26 @@ function onClickButtonPriceDiscount() {
     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
 
     const resultP = document.getElementById("ResultP");
-    resultP.innerText = `El precio con descuento son: $${precioConDescuento}`;
+    resultP.innerText = `El precio con descuento es: $${precioConDescuento}`;
+}
+
+function onClickButtonPriceCoupon() {
+
+    const resultCoupon = document.getElementById("ResultCoupon");
+
+    const inputPrice = document.getElementById("InputPriceCoupon");
+    const priceValue = inputPrice.value;
+
+    const inputCode = document.getElementById("InputCodeCoupon");
+    const codeValue = inputCode.value;
+
+    const infoCoupon = validarCupon(codeValue);
+
+    if (infoCoupon === undefined) {
+        resultCoupon.innerText = "El cupón ingresado no es valido.";
+    } else {
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, infoCoupon.descuento);
+
+        resultCoupon.innerText = `El precio con descuento del ${infoCoupon.descuento}% es: $${precioConDescuento}`;
+    }
 }
